7edd3c92528c0146232b5826e52b140d42dc3a4c
Herbert J. Bernstein <yayahjb@gmail.com>
2012-07-14 21:07:46 +0000

    Correct type on configure script -- HJB

--- BGAOL/config_cgiwrapped_user.sh
+++ BGAOL/config_cgiwrapped_user.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 HTTPDSERVER=`hostname -f`/~${USER};export HTTPDSERVER
-SEARCHURL=http:`hostname -f`/~${USER}/bgaol;export SEARCHURL
+SEARCHURL=http://`hostname -f`/~${USER}/bgaol;export SEARCHURL
 CGIPATH=http://`hostname -f`/cgi-bin/cgiwrap/${USER};export CGIPATH
 FC=g95; export FC
 FFLAGS="-O3"; export FFLAGS
